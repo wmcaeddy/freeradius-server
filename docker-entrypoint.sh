d35ba051e1c02089ae03dfd18c06c93e651916e4
wmcaeddy <wmcaeddy@yahoo.com.hk>
2026-08-20 10:31:32 +0800

    Enable PHP error display for daloRADIUS debugging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,14 @@ set -e
 # Persistent data directory
 mkdir -p /app/data/freeradius/logs /app/data/freeradius/config /app/data/freeradius/certs
 
+# Enable PHP display_errors and log errors to stdout/stderr
+PHP_INI=$(php -i | grep "Loaded Configuration File" | awk '{print $NF}')
+if [ -f "$PHP_INI" ]; then
+    sed -i "s/display_errors = .*/display_errors = On/" "$PHP_INI"
+    sed -i "s/display_startup_errors = .*/display_startup_errors = On/" "$PHP_INI"
+    sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT/" "$PHP_INI"
+fi
+
 # Find and configure all daloRADIUS configuration files
 for DALO_CONF in $(find /var/www/html/daloradius -name "daloradius.conf.php"); do
     sed -i "s/\$configValues\['CONFIG_DB_HOST'\] = .*/\$configValues\['CONFIG_DB_HOST'\] = '${MYSQL_HOST:-localhost}';/" "$DALO_CONF"
