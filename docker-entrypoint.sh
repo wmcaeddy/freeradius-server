69a2fbb5eeddef44b9c9d4fa8b7cb9640e4f437e
wmcaeddy <wmcaeddy@yahoo.com.hk>
2026-08-20 10:56:41 +0800

    Allow searching mariadb schema in contrib/db subfolders

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -30,14 +30,11 @@ if [ -z "$MYSQL_HOST" ] || [ "$MYSQL_HOST" = "localhost" ] || [ "$MYSQL_HOST" = 
         mysql radius < "$FR_SCHEMA" || true
     fi
 
-    # Import ONLY canonical daloRADIUS base schema files (mariadb-daloradius.sql & fr3-mariadb-freeradius.sql)
-    MAIN_SCHEMA=$(find /var/www/html/daloradius/contrib/db -name "mariadb-daloradius.sql" | head -n 1)
-    FR3_SCHEMA=$(find /var/www/html/daloradius/contrib/db -name "fr3-mariadb-freeradius.sql" | head -n 1)
-    DICT_SCHEMA=$(find /var/www/html/daloradius/contrib/db -name "mariadb-daloradius-dictionaries.sql" | head -n 1)
-    
-    [ -n "$FR3_SCHEMA" ] && mysql radius < "$FR3_SCHEMA" || true
-    [ -n "$MAIN_SCHEMA" ] && mysql radius < "$MAIN_SCHEMA" || true
-    [ -n "$DICT_SCHEMA" ] && mysql radius < "$DICT_SCHEMA" || true
+    # Import canonical daloRADIUS tables
+    for SQL_FILE in $(find /var/www/html/daloradius/contrib/db -type f -name "mariadb*.sql" -o -name "fr3-mariadb*.sql" | grep -v "pgsql" | sort); do
+        echo "Importing daloRADIUS schema from $SQL_FILE..."
+        mysql radius < "$SQL_FILE" || true
+    done
 fi
 
 # Enable PHP display_errors and log errors
