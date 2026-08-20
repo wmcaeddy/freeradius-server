8e8cc517feccbb592a0279f8699a9b32fa40000c
wmcaeddy <wmcaeddy@yahoo.com.hk>
2026-08-20 10:23:15 +0800

    Configure Apache DirectoryIndex for daloRADIUS

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,14 @@ set -e
 # Persistent data directory
 mkdir -p /app/data/freeradius/logs /app/data/freeradius/config /app/data/freeradius/certs
 
-# Find and configure daloRADIUS database connection
-DALO_CONF=$(find /var/www/html/daloradius -name "daloradius.conf.php" | head -n 1)
-if [ -n "$DALO_CONF" ] && [ -f "$DALO_CONF" ]; then
+# Find and configure all daloRADIUS configuration files
+for DALO_CONF in $(find /var/www/html/daloradius -name "daloradius.conf.php"); do
     sed -i "s/\$configValues\['CONFIG_DB_HOST'\] = .*/\$configValues\['CONFIG_DB_HOST'\] = '${MYSQL_HOST:-localhost}';/" "$DALO_CONF"
     sed -i "s/\$configValues\['CONFIG_DB_PORT'\] = .*/\$configValues\['CONFIG_DB_PORT'\] = '${MYSQL_PORT:-3306}';/" "$DALO_CONF"
     sed -i "s/\$configValues\['CONFIG_DB_USER'\] = .*/\$configValues\['CONFIG_DB_USER'\] = '${MYSQL_USER:-radius}';/" "$DALO_CONF"
     sed -i "s/\$configValues\['CONFIG_DB_PASS'\] = .*/\$configValues\['CONFIG_DB_PASS'\] = '${MYSQL_PASSWORD:-radius}';/" "$DALO_CONF"
     sed -i "s/\$configValues\['CONFIG_DB_NAME'\] = .*/\$configValues\['CONFIG_DB_NAME'\] = '${MYSQL_DATABASE:-radius}';/" "$DALO_CONF"
-fi
+done
 
 echo "Starting Apache for daloRADIUS Web Admin UI..."
 service apache2 start || apache2ctl start
